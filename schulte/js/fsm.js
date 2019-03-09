91a3b09548609bd3e1c08472da5b31a5b025d81f
Ruslan_fanless <giggles@mail.ru>
2019-03-09 08:46:24 +0500

    new version chulte

---
+++ schulte/js/fsm.js
@@ -0,0 +1,144 @@
+let gameSettings = {
+  size: 3,
+  cellSize: 10,
+  difficulty: "strait",
+  border: 1,
+
+  rate: 0,
+  allClicks: 0,
+  errorClicks: 0,
+  pTime: "",
+
+  level: 0,
+
+}
+
+const LevelProgress = [{
+    id: 1,
+    size: 3,
+    cellSize: 10,
+    difficulty: "strait",
+  },
+  {
+    size: 4,
+    cellSize: 10,
+    difficulty: "strait",
+  },
+  {
+    size: 3,
+    cellSize: 10,
+    difficulty: "rotation",
+  },
+  {
+    size: 4,
+    cellSize: 10,
+    difficulty: "rotation",
+  },
+  {
+    size: 3,
+    cellSize: 10,
+    difficulty: "multicolour",
+  },
+  {
+    size: 4,
+    cellSize: 10,
+    difficulty: "multicolour",
+  },
+]
+
+
+
+var fsm = new StateMachine({
+  init: 'titleScreen',
+  transitions: [{
+      name: 'exitToTitle',
+      from: '*',
+      to: 'titleScreen'
+    },
+    {
+      name: 'playNewGame',
+      from: 'titleScreen',
+      to: 'chooseLevelScreen'
+    },
+    {
+      name: 'continueGame',
+      from: 'titleScreen',
+      to: 'GameScreen'
+    },
+    {
+      name: 'startGame',
+      from: 'chooseLevelScreen',
+      to: 'GameScreen'
+    },
+    {
+      name: 'pauseGame',
+      from: 'GameScreen',
+      to: 'pauseScreen'
+    },
+    {
+      name: 'resumeGame',
+      from: 'pauseScreen',
+      to: 'GameScreen'
+    },
+    {
+      name: 'exitLevel',
+      from: 'pauseScreen',
+      to: 'chooseLevelScreen'
+    },
+    {
+      name: 'exitLevel',
+      from: 'statisticsScreen',
+      to: 'chooseLevelScreen'
+    },
+    {
+      name: 'toTitleScreen',
+      from: 'pauseScreen',
+      to: 'titleScreen'
+    },
+    {
+      name: 'toTitleScreen',
+      from: 'statisticsScreen',
+      to: 'titleScreen'
+    },
+    {
+      name: 'levelFinished',
+      from: 'GameScreen',
+      to: 'statisticsScreen'
+    },
+    {
+      name: 'nextLevel',
+      from: 'statisticsScreen',
+      to: 'GameScreen'
+    },
+    {
+      name: 'restartLevel',
+      from: 'statisticsScreen',
+      to: 'GameScreen'
+    },
+
+
+  ],
+  methods: {
+    onPlayNewGame: function () {
+      chooseLevelScreen();
+    },
+    onStartGame: function () {
+      startGame();
+    },
+    onLevelFinished: function () {
+      finishGame();
+    },
+    onExitToTitle: function () {
+      finishGame();
+    },
+    onExitLevel: function () {
+      chooseLevelScreen();
+    },
+    onRestartLevel: function () {
+      startGame();
+    },
+    onNextLevel: function () {
+      startGame(next = true);
+    }
+  }
+});
---
+++ schulte/js/scenes/statisticsScreen.js
@@ -0,0 +1,38 @@
+
+
+
+
+function finishGame() {
+
+	// clearInterval(time);
+	// document.getElementById('main-table').onclick = '';
+	// btn.value = 'СТАРТ';
+    // counterBtn = !counterBtn;
+    
+    clearInterval(time);
+
+    const stats = `
+    <h1>Статистика по игре</h1>
+            <p>Время: ${gameSettings.pTime}</p>
+            <p>Скорость: ${gameSettings.rate.toFixed(2)} кликов/сек</p>
+            <p>Общее количество кликов: ${gameSettings.allClicks}</p>
+            <p>Количество ошибочных кликов: ${gameSettings.allClicks - gameSettings.size * gameSettings.size }</p>
+            
+            <div id="titlemenu">
+                <button class="button-secondary button-xlarge pure-button" onclick="fsm.restartLevel()">
+                    ЗАНОВО
+                </button>
+                <button class="button-secondary button-xlarge pure-button" onclick="fsm.nextLevel()">
+                    СЛЕДУЮЩИЙ
+                </button>
+                <button class="button-secondary button-xlarge pure-button" onclick="fsm.exitLevel()">
+                    ВЫБРАТЬ УРОВЕНЬ
+                </button>
+                <button class="button-secondary button-xlarge pure-button">
+                    В МЕНЮ 
+                </button>
+            </div>
+    `
+    
+    $( ".screen" ).html(stats );
+}
