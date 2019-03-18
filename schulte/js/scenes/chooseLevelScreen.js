4610c2c503a1aac04ed52ad8b8cd477580e2bb65
Ruslan_fanless <giggles@mail.ru>
2019-03-18 17:08:03 +0500

    w shulte

--- schulte/js/scenes/chooseLevelScreen.js
+++ schulte/js/scenes/chooseLevelScreen.js
@@ -1,89 +1,159 @@
-
-
-
-
-
-
-
-
-
 function chooseLevelScreen() {
-
-    const ChooseLevelHTML = `
-    <div id="titlemenu">
-        <form class="pure-form pure-form-stacked" action="" onsubmit="return false">
-            <select name="size" id="size" required onchange="myFunction()">
-                <option disabled value="">Размер</option>
-                <option selected value="2">&nbsp;2</option>
-                <option value="3">&nbsp;3</option>
-                <option value="4">&nbsp;4</option>
-                <option value="5">&nbsp;5</option>
-                <option value="6">&nbsp;6</option>
-                <option value="7">&nbsp;7</option>
-                <option value="8">&nbsp;8</option>
-            </select>
-            <select name="difficulty" id="difficulty" required onchange="myFunction()">
-                <option disabled value="">Сложность</option>
-                <option selected value="strait">Обычный</option>
-                <option value="rotation">Поворот на 90</option>
-                <option value="multicolour">Цветные</option>
-            </select>
-            <select name="cellsize" id="cellsize" required onchange="myFunction()">
-                <option disabled value="">Размер клеток</option>
-                <option value="0">Самый маленький</option>
-                <option selected value="10">Маленький</option>
-                <option value="20">Средний</option>
-                <option value="30">Большой</option>
-                <option value="40">Огромный</option>
-            </select>
-            <select name="border" id="border" required onchange="myFunction()">
-                <option disabled value="">Границы</option>
-                <option selected value="1">С границами</option>
-                <option value="2">Без границ</option>
-            </select>
-           
-            
-            
-        </form>
-        <button class="button-secondary button-xlarge pure-button" onclick="fsm.startGame()">СТАРТ!</button>
+	const NewMenu = `
+  <div class="container">
+  <hr>
+		<div class="row">
+			<div class="col text-center">
+        
+        <div class="btn-group btn-group-toggle btn-group-justified my-grid-size btn-block" data-toggle="buttons">
+					<label class="btn btn-primary disabled">
+						Размер
+					</label>
+					<label class="btn btn-primary">
+						<input type="radio" name="size" value="3" autocomplete="off"> 3x3
+					</label>
+					<label class="btn btn-primary">
+						<input type="radio" name="size" value="4" autocomplete="off"> 4x4
+					</label>
+					<label class="btn btn-primary">
+						<input type="radio" name="size" value="5" autocomplete="off"> 5x5
+					</label>
+					<label class="btn btn-primary">
+						<input type="radio" name="size" value="6" autocomplete="off"> 6x6
+					</label>
+					<label class="btn btn-primary">
+						<input type="radio" name="size" value="7" autocomplete="off"> 7x7
+					</label>
+        </div>
+      </div>
     </div>
-    `
-const levelButton = `
-<button class="button-secondary button-xlarge pure-button  pure-button-disabled" onclick=""> Простой 3х3 <br>
-    <span class="fa fa-star checked"></span>
-    <span class="fa fa-star checked"></span>
-    <span class="fa fa-star"></span>
-</button>
+    <br />
+		<div class="row">
+			<div class="col text-center">
+        <div class="btn-group btn-group-toggle btn-group-justified my-grid-operation btn-block" data-toggle="buttons">
+					<label class="btn btn-primary disabled">
+						Операции
+					</label>
+					<label class="btn btn-primary">
+					  <img src="img/number-five-in-circular-button.svg" height="40px" alt="number 5 icon"><br />
+						<input type="radio" name="operation" value="standard" autocomplete="off"> Станд. числа
+					</label>
+					<label class="btn btn-primary">
+					<img src="img/number-five-in-circular-button.svg" height="30px"  alt="number 5 icon" style="transform: rotate(180deg);"><br />
+
+					
+						<input type="radio" name="operation" value="rotated" autocomplete="off"> Числа перевертыши
+					</label>
+					<label class="btn btn-primary">
+					<img src="img/rgb.svg" height="40px" alt="rgb icon"><br />
+						<input type="radio" name="operation" value="colored" autocomplete="off"> Цветные
+					</label>
+					<label class="btn btn-primary">
+					<img src="img/abc-block.svg" height="40px"alt="alphabet icon"><br />
+						<input type="radio" name="operation" value="alphabet" autocomplete="off" > Алфавит
+					</label>
+					<label class="btn btn-primary">
+					
+					<img src="img/letter_number.svg" height="40px"  alt="letter number icon"><br />
+
+						<input type="radio" name="operation" value="alphabet_numbers" autocomplete="off"> Алфавит+числа
+					</label>
+        </div>
+			</div>
+    </div>
+    <br />
+    <div class="row">
+			<div class="col text-center">
+        <div class="btn-group btn-group-toggle btn-group-justified my-grid-order btn-block" data-toggle="buttons">
+					<label class="btn btn-primary disabled">
+						Порядок
+					</label>
+					<label class="btn btn-primary">
+						<input type="radio" name="order" value="direct" autocomplete="off"> Прямой
+					</label>
+					<label class="btn btn-primary">
+						<input type="radio" name="order" value="reverse" autocomplete="off"> Обратный
+					</label>
+        </div>
+			</div>
+    </div>
+
+    <div class="row">
+			<div class="col text-center">
+        <div class="btn-group btn-group-toggle btn-group-justified my-grid-timer btn-block" data-toggle="buttons">
+					<label class="btn btn-primary disabled">
+						Время
+					</label>
+					<label class="btn btn-primary">
+						<input type="radio" name="timer" value="notimelimit" autocomplete="off"> Без дедлайна
+					</label>
+					<label class="btn btn-primary">
+						<input type="radio" name="timer" value="timelimit" autocomplete="off"> С дедлайном
+					</label>
+        </div>
+			</div>
+		</div>
+		
+		<hr>
+
+    <div class="row">
+			<div class="col text-center">
+    		<button type="button" class="btn btn-success btn-lg my-start">СТАРТ!</button>
+				<hr>
+			</div>
+		</div>
+		
+	</div>
+`
+const upperMenu = `
+<nav class="navbar navbar-light">
+<h1><a class="navbar-brand" href="#">Таблицы Шульте</a></h1>
+</nav>
 `
 
+	$(".uppermenu").html(upperMenu);
+	$(".screen").html(NewMenu);
+	toggleMenu();
+	updateMenuWithSettingsFormConfig();
 
-const LevelMap = `
-<div class="wrapper">
-  <div class="box">1</div>
-  <div class="box">${levelButton}</div>
-  <div class="box">3</div>
-  <div class="box">4</div>
-  <div class="box">5</div>
-  <div class="box">6</div>
-</div>
+	$('.my-start').on('click', function () {
+		gameSettings.size = $('.my-grid-size > label.active input[value]').val();
+		gameSettings.operation = $('.my-grid-operation > label.active input[value]').val();
+		gameSettings.order = $('.my-grid-order > label.active input[value]').val();
+		gameSettings.timer = $('.my-grid-timer > label.active input[value]').val();
 
-`
-
-
-
-
-$( ".screen" ).html( ChooseLevelHTML );
-
-
-
+		startGameF();
+	});
 }
 
-function myFunction() {
-    gameSettings.size = document.getElementById('size').options[size.selectedIndex].value;
-    
-    gameSettings.cellSize = document.getElementById('cellsize').options[cellsize.selectedIndex].value;
-    gameSettings.border = document.getElementById('border').options[border.selectedIndex].value;
-    gameSettings.difficulty = document.getElementById('difficulty').options[difficulty.selectedIndex].value
+function updateMenuWithSettingsFormConfig() {
 
-    console.log(gameSettings)
+	$(".my-grid-size input[name='size']").each(function (index) {
+
+		if ($(this)[0].value === gameSettings.size.toString()) {
+			$(this).parent().addClass("active")
+		}
+	});
+
+	$(".my-grid-operation input[name='operation']").each(function (index) {
+
+		if ($(this)[0].value === gameSettings.operation.toString()) {
+			$(this).parent().addClass("active")
+		}
+	});
+
+	$(".my-grid-order input[name='order']").each(function (index) {
+
+		if ($(this)[0].value === gameSettings.order.toString()) {
+			$(this).parent().addClass("active")
+		}
+	});
+
+	$(".my-grid-timer input[name='timer']").each(function (index) {
+
+		if ($(this)[0].value === gameSettings.timer.toString()) {
+			$(this).parent().addClass("active")
+		}
+	});
+
 }
--- schulte/js/scenes/gameScreen.js
+++ schulte/js/scenes/gameScreen.js
@@ -1,301 +1,279 @@
+function restartGameF() {
+	let size = $(".size-select option:selected").val();
+	let operation = $(".operation-select option:selected").val();
+	gameSettings.order = $(".order-select option:selected").val();
+	gameSettings.timer = $(".timer-select option:selected").val();
+	gameSettings.size = size;
+	gameSettings.operation = operation;
+	clearInterval(0);
+	startGameF();
 
-
-function startGame(next) {
-
-	const GameHTML = `
-
-<div id="game">
-	<div class="main">
-		<img class="left" src="img/animal1.png" />  
-		
-		<div class="undertable">
-		</div>
-
-		<img class="right" src="img/animal2.png" />  
-		
-	</div>
-	<div class="gamebuttons">
-	
-		<button class="button-secondary button-xlarge pure-button" onclick="fsm.restartLevel()"><i class="fas fa-undo"></i> РЕСТАРТ</button>
-		<button class="button-secondary button-xlarge pure-button" onclick="fsm.playNewGame()"><i class="fas fa-sliders-h"></i> В МЕНЮ</button>
-
-	</div>
-</div>
-
-`
-	$(".screen").html(GameHTML);
-
-
-if (next === true) {
-	gameSettings.level++;
-	gameSettings.size = LevelProgress[gameSettings.level].size;
-	gameSettings.difficulty = LevelProgress[gameSettings.level].difficulty;
 }
 
+function startGameF() {
 
 
-	let main = document.querySelector('.undertable');
-	let counter = 0;
-	let counterBtn = true;
-	let counterTimeMinute = 0;
-	let counterTimeSeconds = 0;
+	const uppermenu = `
+	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+	<button class="btn btn-primary my-gotomenu">МЕНЮ</button>
+	
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+	</button>
+	
+  <div class="collapse navbar-collapse" id="navbarNav">
+		<ul class="navbar-nav">
+			<li class="nav-item">
+				<button class="btn btn-success my-launch">РЕСТАРТ</button>
+			</li>
+      <li class="nav-item">
+		<select class="custom-select size-select">
+			<option value="3">Размер: 3</option>
+			<option value="4">Размер: 4</option>
+			<option value="5">Размер: 5</option>
+			<option value="6">Размер: 6</option>
+			<option value="7">Размер: 7</option>
+		</select>
+      </li>
+      <li class="nav-item">
+				<select class="custom-select operation-select">
+					<option value="standard">Стандартные числа</option>
+					<option value="rotated">Числа перевертыши</option>
+					<option value="colored">Цветные числа</option>
+					<option value="alphabet">Алфавит</option>
+					<option value="alphabet_numbers">Алфавит + числа</option>
+				</select>
+			</li>
+      <li class="nav-item">
+					<select class="custom-select order-select ">
+						<option value="direct">Прямой</option>
+						<option value="reverse">Обратный</option>
+					</select>
+			</li>
+      <li class="nav-item">
+					<select class="custom-select timer-select ">
+						<option value="notimelimit">Без дедлайна</option>
+						<option value="timelimit">С дедлайном</option>
+					</select>
+			</li>
+		</ul>
+  </div>
+</nav>
+`
+
+	const progressbar = `
+<div class="progress" style="height: 3px;">
+<div class="progress-bar" role="progressbar" style="width: 100%;" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+</div>
+`
+
+	const NewGame = `
+	<br />
+	<div class="stretchy-wrapper d-block mx-auto">
+	${gameSettings.timer === "timelimit" ? progressbar : ""}
+		<div id="grid">
+
+		</div>
+	</div>
+`
+
+	$(".uppermenu").html(uppermenu);
+	$(".screen").html(NewGame);
+
+	setSquareSize();
+	updateUpperMenuWithConfig();
+
+	$('#navbarNav').on('change', function () {
+		getReady();
+	});
+
+	function getReady() {
+		$(".my-launch").html("СТАРТ");
+		clearInterval(time);
+		$("#grid").html(`<button class="btn btn-lg my-launch" style="background-color:transparent">
+	<img src="img/play-button.svg" height="50%" alt="play"><br />
+	</button>`)
+		$('.my-launch').on('click', function () {
+			restartGameF();
+		});
+		$("#grid").css({
+			"grid-template-rows": `1fr`,
+			"grid-template-columns": `1fr`,
+		})
+	}
+
+	$('.my-launch').on('click', function () {
+		clearInterval(time);
+		restartGameF();
+	});
+
+	$('.my-gotomenu').on('click', function () {
+		clearInterval(time);
+		chooseLevelScreen();
+	});
+
 	let counterTimeMs = 0;
-	let time = 0;
-	let clicks = 0;
 	gameSettings.allClicks = 0;
-	let successClicks = 0;
-	let lastnumber = 0;
-	let menu_time = document.querySelector('.menu-time');
-	let statistics = document.querySelector('.statistics');
-
-	let sizeWithValue = gameSettings.size;
-	let cellSizeWithValue = gameSettings.cellSize;
-	let borderWithValue = gameSettings.border;
-	let typeWithValue = 1;
-	let difficultyWithValue = gameSettings.difficulty;
-
+	let time = 0;
+	const firstRULetterCode = 1040;
+	// const locale = (navigator.languages && navigator.languages.length) ? navigator.languages[0] : navigator.language;
+	const sizeWithValue = gameSettings.size;
+	const gridSize = sizeWithValue * sizeWithValue;
+	let tlimit = 0;
+	if (sizeWithValue == 3) {
+		tlimit = 10 * 1000;
+	} else if (sizeWithValue == 4) {
+		tlimit = 15 * 1000;
+	} else if (sizeWithValue == 5) {
+		tlimit = 30 * 1000;
+	} else if (sizeWithValue == 6) {
+		tlimit = 45 * 1000;
+	} else if (sizeWithValue == 7) {
+		tlimit = 60 * 1000;
+	}
+	let difficultyWithValue = gameSettings.operation;
+	let lettersWithIndexes = [];
+	let lettersAndNumbersWithIndexes = [];
 	const rotArray = [0, 90, 180, 270];
-	if (!typeWithValue || !borderWithValue || !cellSizeWithValue || !sizeWithValue || !difficultyWithValue) {
-		alert("fill all values");
-	} else {
-		let arrayWithNumbers = [];
-		let arrayWithLetters = [
-			'a',
-			'b',
-			'c',
-			'd',
-			'e',
-			'f',
-			'g',
-			'h',
-			'i',
-			'j',
-			'k',
-			'l',
-			'm',
-			'n',
-			'o',
-			'p',
-			'r',
-			's',
-			't',
-			'u',
-			'v',
-			'w',
-			'x',
-			'y',
-			'z'
-		];
-		let counterOfWhiles = 0;
-		let tab = document.getElementsByTagName('table');
-		let td = document.getElementsByTagName('td');
-		let tr = document.getElementsByTagName('tr');
-		let pTime = document.getElementById('time');
-		let cl = document.getElementById('clicks');
-		let next = document.getElementById('next');
-		let clickspeed = document.getElementById('clickspeed');
-
-
-		// Add numbers to array
-		for (let i = 1; i <= sizeWithValue * sizeWithValue; i++) {
-			arrayWithNumbers[i] = i;
-		}
-
-		// Select a size of table with letters to randomize
-		let size_of_letters = sizeWithValue * sizeWithValue;
-		arrayWithLetters.splice(size_of_letters, 24);
-
-		// Sort random table
-		arrayWithNumbers.sort(() => Math.random() - 0.5);
-		arrayWithLetters.sort(() => Math.random() - 0.5);
-
-
-		// Generate table
-		function generateTable() {
-			let table = document.createElement('table');
-			table.setAttribute('id', 'main-table');
-			startTime = +new Date();
-			table.onclick = function (obj) {
-				clicks++;
-				gameSettings.allClicks++;
-			//	cl.innerHTML = "Клики: " + clicks;
-
-				if (lastnumber + 1 == arrayWithNumbers.length - 1) {
-					successClicks++;
-					clickTime = +new Date();
-					let rate = successClicks / (clickTime - startTime) * 1000.0;
-					gameSettings.rate = rate;
-				//	clickspeed.innerHTML = rate.toFixed(2) + " кликов/сек";
-					clearInterval(time);
-					fsm.levelFinished();
-				} else {
-					let clickednumber = parseInt(obj.path[0].id);
-
-					if (clickednumber == lastnumber + 1) {
-
-						successClicks++;
-
-						lastnumber = clickednumber;
-						let expectednumber = lastnumber + 1
-					//	next.innerHTML = "Найдите: " + expectednumber;
-					}
-				}
-
-			};
-			for (let i = 0; i < sizeWithValue; i++) {
-				let row = document.createElement('tr');
-				row.setAttribute('class', 'row_list');
-				for (let j = 0; j < sizeWithValue; j++) {
-					let cell = document.createElement('td');
-					if (difficultyWithValue == "rotation") {
-						var rand = rotArray[Math.floor(Math.random() * rotArray.length)];
-						cell.setAttribute('class', 'cell_list rotate_' + rand);
-					} else if (difficultyWithValue === "strait") {
-						cell.setAttribute('class', 'cell_list');
-					} else if (difficultyWithValue === "multicolour") {
-						var randColor = ColorsList[Math.floor(Math.random() * ColorsList.length)];
-						console.log(randColor)
-						cell.setAttribute('class', 'cell_list');
-						cell.style.color = randColor;
-					}
-					let appendtext = "";
-					if (typeWithValue == 1) {
-						if(difficultyWithValue == "rotation" && (arrayWithNumbers[counterOfWhiles] === 6 || arrayWithNumbers[counterOfWhiles] === 9)) {
-							 appendtext = "."
-						} else {
-							 appendtext = ""
-						}
-						let number = document.createTextNode(arrayWithNumbers[counterOfWhiles] + appendtext);
-						cell.setAttribute('id', arrayWithNumbers[counterOfWhiles]);
-						counterOfWhiles++;
-						cell.appendChild(number);
-
-						row.appendChild(cell);
-					} else if (typeWithValue == 2) {
-						let letter = document.createTextNode(arrayWithLetters[counterOfWhiles]);
-						counterOfWhiles++;
-						cell.appendChild(letter);
-						row.appendChild(cell);
-					}
-				}
-				table.appendChild(row);
-			}
-			main.appendChild(table);
-			counter++;
-		}
-
-
-
-		// Set a value of border style
-		function setValueOfBorder() {
-			if (borderWithValue == 2) {
-				for (let i = tab.length - 1; i >= 0; i--) {
-					tab[i].style.border = '1px solid white';
-				}
-				for (let i = td.length - 1; i >= 0; i--) {
-					td[i].style.border = '1px solid white';
-				}
-				for (let i = tr.length - 1; i >= 0; i--) {
-					tr[i].style.border = '1px solid white';
-				}
-			}
-		}
-
-		// Set a value of cell size
-		function setValueOfCell() {
-			if (cellSizeWithValue > 0) {
-				for (let i = 1; i < 5; i++) {
-					if (cellSizeWithValue == i * 10) {
-						for (let j = tab.length - 1; j >= 0; j--) {
-							tab[j].style.width = i * 120 + 300 + 'px';
-							tab[j].style.height = i * 120 + 300 + 'px';
-							tab[j].style.tableLayout = 'fixed';
-						}
-					}
-				}
-			}
-		}
-
-		// Removing table every click on button
-		function removingTableEveryClick() {
-			if (counter > 0) {
-				for (let i = tab.length - 1; i >= 0; i--) {
-					tab[i].remove();
-				}
-			}
-		}
-
-		// Timer
-
-		function timer() {
-			time = setInterval(clockUp, 10);
-		}
-
-		function clockUp() {
-			if (counterTimeMs == 100) {
-				counterTimeSeconds++;
-				counterTimeMs = 0;
-			}
-			if (counterTimeSeconds == 60) {
-				counterTimeMinute++;
-				counterTimeSeconds = 0;
-			}
-			counterTimeMs++;
-			
-			gameSettings.pTime = `0${counterTimeMinute} : ${counterTimeSeconds} : ${counterTimeMs}`;
-/* 			if (counterTimeSeconds < 10) {
-				pTime.innerHTML = `0${counterTimeMinute} : 0${counterTimeSeconds} : ${counterTimeMs}`;
-			} else if (counterTimeSeconds >= 10) {
-				pTime.innerHTML = `0${counterTimeMinute} : ${counterTimeSeconds} : ${counterTimeMs}`;
-			}
-
-			if (counterTimeMinute >= 1 && counterTimeSeconds < 10) {
-				pTime.innerHTML = `0${counterTimeMinute} : 0${counterTimeSeconds} : ${counterTimeMs}`;
-			} else if (counterTimeMinute >= 1 && counterTimeSeconds >= 10) {
-				pTime.innerHTML = `0${counterTimeMinute} : ${counterTimeSeconds} : ${counterTimeMs}`;
-			}
-
-			if (counterTimeMinute >= 10 && counterTimeSeconds < 10) {
-				pTime.innerHTML = `${counterTimeMinute} : 0${counterTimeSeconds} : ${counterTimeMs}`;
-			} else if (counterTimeMinute >= 10 && counterTimeSeconds >= 10) {
-				pTime.innerHTML = `${counterTimeMinute} : ${counterTimeSeconds} : ${counterTimeMs}`;
-			} */
-
-			clickTime = +new Date();
-			let rate = successClicks / (clickTime - startTime) * 1000.0;
-		//	clickspeed.innerHTML = rate.toFixed(2) + " кликов/сек";
-		}
-
-		if (counterBtn) {
-			//menu_time.style.display = 'flex';
-			//statistics.style.display = 'flex';
-			//btn.value = "СТОП";
-			counterBtn = !counterBtn;
-			// call the function
-			clicks = 0;
-			lastnumber = 0;
-			successClicks = 0;
-			//next.innerHTML = "Найдите: 1";
-		//	cl.innerHTML = "Клики: " + clicks;
-			removingTableEveryClick();
-			generateTable();
-			setValueOfBorder();
-			setValueOfCell();
-			timer();
+	let arrayWithLetters = [];
+	let arrayWithNumbers = [];
+	/* 		const arrayWithLetters = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖДИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'.split(''); */
+	for (let i = 0; i < gridSize; i++) {
+		lettersWithIndexes.push([i + 1, String.fromCharCode(firstRULetterCode + i)])
+	}
+	let index = 1;
+	for (let i = 1; i <= gridSize; i++) {
+		if (i % 2 == 1) {
+			lettersAndNumbersWithIndexes.push([i, index])
+			lettersAndNumbersWithIndexes.push([i + 1, String.fromCharCode(firstRULetterCode + index - 1)])
 		} else {
-			//btn.value = 'СТАРТ';
-			counterBtn = !counterBtn;
-			clearInterval(time);
-			counterTimeMinute = 0;
-			counterTimeMs = 0;
-			counterTimeSeconds = 0;
+			index++;
 		}
 	}
+	lettersAndNumbersWithIndexes.splice(gridSize, lettersAndNumbersWithIndexes.length - gridSize);
+	// var lettersWithIndexes = arrayWithLetters.map( i => [i, arrayWithLetters.indexOf(i) + 1] )
+	let counterOfWhiles = 0;
+	// Add numbers to array
+	for (let i = 1; i <= sizeWithValue * sizeWithValue; i++) {
+		arrayWithNumbers[i] = i;
+	}
+	// Select a size of table with letters to randomize
+	let size_of_letters = sizeWithValue * sizeWithValue;
+	arrayWithLetters.splice(size_of_letters, 24);
+	// Sort random table
+	arrayWithNumbers.sort(() => Math.random() - 0.5);
+	arrayWithLetters.sort(() => Math.random() - 0.5);
+	lettersWithIndexes.sort(() => Math.random() - 0.5);
+	lettersAndNumbersWithIndexes.sort(() => Math.random() - 0.5);
+
+	function timer() {
+		time = setInterval(clockUp, 100);
+	}
+
+	function clockUp() {
+		counterTimeMs = counterTimeMs + 100;
+		let valeur = Math.floor((tlimit - counterTimeMs) / tlimit * 100);
+		if (valeur <= 0) {
+			getReady();
+		}
+		$('.progress-bar').css('width', valeur + '%').attr('aria-valuenow', valeur);
+	}
+
+	function generateGrid() {
+		let grid = document.getElementById('grid');
+		startTime = +new Date();
+		for (let i = 0; i < gridSize; i++) {
+			let griddiv = document.createElement('div');
+			if (difficultyWithValue == "rotated") {
+				var rand = rotArray[Math.floor(Math.random() * rotArray.length)];
+				griddiv.setAttribute('class', 'cell_list rotate_' + rand);
+			} else if (difficultyWithValue === "colored") {
+				var randColor = ColorsList[Math.floor(Math.random() * ColorsList.length)];
+				griddiv.style.color = randColor;
+			}
+			let appendtext = "";
+			if (difficultyWithValue == "rotated" || difficultyWithValue == "colored" || difficultyWithValue == "standard") {
+				if (difficultyWithValue == "rotated" & (arrayWithNumbers[counterOfWhiles] === 6 || arrayWithNumbers[counterOfWhiles] === 9)) {
+					appendtext = "."
+				}
+				let number = document.createTextNode(arrayWithNumbers[counterOfWhiles] + appendtext);
+				griddiv.setAttribute('id', arrayWithNumbers[counterOfWhiles]);
+				counterOfWhiles++;
+				griddiv.appendChild(number);
+			} else if (difficultyWithValue == "alphabet") {
+				let letter = document.createTextNode(lettersWithIndexes[counterOfWhiles][1]);
+				griddiv.setAttribute('id', lettersWithIndexes[counterOfWhiles][0]);
+				counterOfWhiles++;
+				griddiv.appendChild(letter);
+			} else if (difficultyWithValue == "alphabet_numbers") {
+				let letter = document.createTextNode(lettersAndNumbersWithIndexes[counterOfWhiles][1]);
+				griddiv.setAttribute('id', lettersAndNumbersWithIndexes[counterOfWhiles][0]);
+				counterOfWhiles++;
+				griddiv.appendChild(letter);
+
+			}
+			grid.appendChild(griddiv);
+		}
+
+		$("#grid").css({
+			"grid-template-rows": `repeat(${sizeWithValue}, 1fr)`,
+			"grid-template-columns": `repeat(${sizeWithValue}, 1fr)`,
+		})
+
+		let prevNumber = gameSettings.order === "direct" ? 0 : gridSize + 1;
+		let goalNumber = gameSettings.order === "direct" ? gridSize + 1 : 0;
+		let signed1 = gameSettings.order === "direct" ? 1 : -1;
+
+		$('#grid').on('click', 'div', function () {
+			gameSettings.allClicks++;
+			let clickedNumber = parseInt($(this).attr('id'));
+			if (clickedNumber === prevNumber + signed1) {
+				successClicks++;
+				if (prevNumber === goalNumber - 2 * signed1) {
+					clickTime = +new Date();
+
+					gameSettings.pTime = clickTime - startTime;
+					clearInterval(time);
+					viewStats();
+				}
+				prevNumber = clickedNumber;
+			}
+		});
+	}
+
+	clicks = 0;
+	lastnumber = 0;
+	successClicks = 0;
+	generateGrid();
+	if (gameSettings.timer == "timelimit") {
+		timer();
+	}
+
 }
+
 
 
 function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+function updateUpperMenuWithConfig() {
+
+	$(`.size-select option[value=${gameSettings.size}]`).each(function (index) {
+		$(this).prop('selected', true);
+	});
+	$(`.operation-select option[value=${gameSettings.operation}]`).each(function (index) {
+		$(this).prop('selected', true);
+	});
+	$(`.order-select option[value=${gameSettings.order}]`).each(function (index) {
+		$(this).prop('selected', true);
+	});
+	$(`.timer-select option[value=${gameSettings.timer}]`).each(function (index) {
+		$(this).prop('selected', true);
+	});
+}
+
+
+startGameF();
