cdfd756ec9843239eb7ed609db775c81ddd036c3
Ruslan_fanless <giggles@mail.ru>
2019-05-06 17:55:18 +0500

    commit in test

---
+++ test/precache-manifest.87c21f76b88d88906dd78784f9d4dbd8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3919cfb26bed460f076267ed27caae9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "95812521ceefcc82defc",
+    "url": "/static/css/main.c9aa7376.chunk.css"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "95812521ceefcc82defc",
+    "url": "/static/js/main.432075fd.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
